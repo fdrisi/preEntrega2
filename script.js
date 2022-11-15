ec9fe79d8332e74d6c9f9f83851f9186b54de83b
Federico Risi <damianrisi@gmail.com>
2022-11-15 11:49:20 -0300

    segunda preentrega

---
+++ script.js
@@ -0,0 +1,525 @@
+seleccionados = [
+    {
+        nombre: "Damián Emiliano", 
+        apellido: "Martínez",
+        edad: 30, 
+        anioNacimiento: 1992,
+        altura: 1.95, 
+        clubActual: "Aston Villa",
+        clubDebut: "Oxford United",
+        paisDebut: "Inglaterra",
+        anioDebut: 2012,
+        debutSeleccion: 2021,
+        partidosSeleccion: 18,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion: "Arquero", 
+        dorsalMundialista: 23
+        },
+    {
+        nombre: "Gerónimo", 
+        apellido: "Rulli", 
+        edad: 30, 
+        anioNacimiento: 1992,
+        altura: 1.89, 
+        clubActual: "Villarreal", 
+        clubDebut: "Estudiantes de La Plata",
+        paisDebut: "Argentina",
+        anioDebut: 2013,
+        debutSeleccion: 2018,
+        partidosSeleccion: 4,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion: "Arquero",
+        dorsalMundialista: 12
+    },
+    {
+        nombre: "Franco", 
+        apellido: "Armani",
+        edad: 36, 
+        anioNacimiento: 1986,
+        altura: 1.89, 
+        clubActual: "River Plate", 
+        clubDebut: "Ferro Carril Oeste",
+        paisDebut: "Argentina",
+        anioDebut: 2008,
+        debutSeleccion: 2018,
+        partidosSeleccion: 18,
+        golesSeleccion: 0,
+        partidosMundiales: 2,
+        posicion: "Arquero",
+        dorsalMundialista: 1
+    },
+    {
+        nombre: "Nahuel", 
+        apellido: "Molina", 
+        edad: 24, 
+        anioNacimiento: 1998,
+        altura: 1.75, 
+        clubActual: "Atlético de Madrid", 
+        clubDebut: "Boca Juniors",
+        paisDebut: "Argentina",
+        anioDebut: 2016,
+        debutSeleccion: 2021,
+        partidosSeleccion: 19,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion:"Defensor" ,
+        dorsalMundialista: 26
+    },
+    {
+        nombre: "Gonzalo", 
+        apellido: "Montiel", 
+        edad: 25, 
+        anioNacimiento: 1997,
+        altura: 1.75, 
+        clubActual: "Sevilla", 
+        clubDebut: "River Plate",
+        paisDebut: "Argentina",
+        anioDebut: 2016,
+        debutSeleccion: 2019,
+        partidosSeleccion: 17,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion:"Defensor" ,
+        dorsalMundialista: 4
+    },
+    {
+        nombre: "Cristian", 
+        apellido: "Romero", 
+        edad: 24, 
+        anioNacimiento: 1998,
+        altura: 1.85, 
+        clubActual: "Tottenham", 
+        clubDebut: "Belgrano",
+        paisDebut: "Argentina",
+        anioDebut: 2016,
+        debutSeleccion: 2021,
+        partidosSeleccion: 12,
+        golesSeleccion: 1,
+        partidosMundiales: 0,
+        posicion:"Defensor" ,
+        dorsalMundialista: 13
+    },
+    {
+        nombre: "Germán", 
+        apellido: "Pezzella", 
+        edad: 31, 
+        anioNacimiento: 1991,
+        altura: 1.87, 
+        clubActual: "Betis", 
+        clubDebut: "River Plate",
+        paisDebut: "Argentina",
+        anioDebut: 2011,
+        debutSeleccion: 2017,
+        partidosSeleccion: 31,
+        golesSeleccion: 2,
+        partidosMundiales: 0,
+        posicion:"Defensor",
+        dorsalMundialista:6
+     },
+    {
+        nombre: "Nicolás", 
+        apellido: "Otamendi", 
+        edad: 34, 
+        anioNacimiento: 1988,
+        altura: 1.83, 
+        clubActual: "Benfica", 
+        clubDebut: "Vélez Sarsfield",
+        paisDebut: "Argentina",
+        anioDebut: 2008,
+        debutSeleccion: 2009,
+        partidosSeleccion: 92,
+        golesSeleccion: 4,
+        partidosMundiales: 7,
+        posicion:"Defensor",
+        dorsalMundialista:19
+    },
+    {
+        nombre: "Lisandro", 
+        apellido: "Martínez", 
+        edad: 24, 
+        anioNacimiento: 1998,
+        altura: 1.75, 
+        clubActual: "Manchester United", 
+        clubDebut: "Newells Old Boys",
+        paisDebut: "Argentina",
+        anioDebut: 2017,
+        debutSeleccion: 2019,
+        partidosSeleccion: 9,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion:"Defensor",
+        dorsalMundialista:25
+    },
+    {
+        nombre: "Marcos", 
+        apellido: "Acuña", 
+        edad: 31, 
+        anioNacimiento: 1991,
+        altura: 1.72, 
+        clubActual: "Sevilla", 
+        clubDebut: "Ferro Carril Oeste",
+        paisDebut: "Argentina",
+        anioDebut: 2011,
+        debutSeleccion: 2016,
+        partidosSeleccion: 42,
+        golesSeleccion: 0,
+        partidosMundiales: 1,
+        posicion:"Defensor",
+        dorsalMundialista:8
+    },
+    {
+        nombre: "Nicolás", 
+        apellido: "Tagliafico", 
+        edad: 30, 
+        anioNacimiento: 1991,
+        altura: 1.72, 
+        clubActual: "Olympique de Lyon", 
+        clubDebut: "Banfield",
+        paisDebut: "Argentina",
+        anioDebut: 2011,
+        debutSeleccion: 2017,
+        partidosSeleccion: 42,
+        golesSeleccion: 0,
+        partidosMundiales: 4,
+        posicion:"Defensor" ,
+        dorsalMundialista:3
+    },
+    {
+        nombre: "Juan", 
+        apellido: "Foyth", 
+        edad: 24, 
+        anioNacimiento: 1998,
+        altura: 1.87, 
+        clubActual: "Villarreal", 
+        clubDebut: "Estudiantes de La Plata",
+        paisDebut: "Argentina",
+        anioDebut: 2017,
+        debutSeleccion: 2018,
+        partidosSeleccion: 15,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion:"Defensor" ,
+        dorsalMundialista:2
+    },
+    {
+        nombre: "Rodrigo", 
+        apellido: "De Paul", 
+        edad: 28, 
+        anioNacimiento: 1994,
+        altura: 1.80, 
+        clubActual: "Atlético de Madrid", 
+        clubDebut: "Racing Club",
+        paisDebut: "Argentina",
+        anioDebut: 2013,
+        debutSeleccion: 2018,
+        partidosSeleccion: 43,
+        golesSeleccion: 2,
+        partidosMundiales: 0,
+        posicion: "Mediocampista",
+        dorsalMundialista:7
+    },
+    {
+        nombre: "Leandro", 
+        apellido: "Paredes", 
+        edad: 28, 
+        anioNacimiento: 1994,
+        altura: 1.80, 
+        clubActual: "Juventus", 
+        clubDebut: "Boca Juniors",
+        paisDebut: "Argentina",
+        anioDebut: 2010,
+        debutSeleccion: 2017,
+        partidosSeleccion: 45,
+        golesSeleccion: 4,
+        partidosMundiales: 0,
+        posicion: "Mediocampista",
+        dorsalMundialista:5
+    },
+    {
+        nombre: "Alexis", 
+        apellido: "Mac Allister", 
+        edad: 23, 
+        anioNacimiento: 1998,
+        altura: 1.75, 
+        clubActual: "Brighton", 
+        clubDebut: "Argentinos Juniors",
+        paisDebut: "Argentina",
+        anioDebut: 2016,
+        debutSeleccion: 2019,
+        partidosSeleccion: 7,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion: "Mediocampista",
+        dorsalMundialista:17
+    },
+    {
+        nombre: "Guido", 
+        apellido: "Rodríguez", 
+        edad: 28, 
+        anioNacimiento: 28,
+        altura: 1.85, 
+        clubActual: "Betis", 
+        clubDebut: "River Plate",
+        paisDebut: "Argentina",
+        anioDebut: 2014,
+        debutSeleccion: 2017,
+        partidosSeleccion: 25,
+        golesSeleccion: 1,
+        partidosMundiales: 0,
+        posicion: "Mediocampista",
+        dorsalMundialista:18
+    },
+    {
+        nombre: "Alejandro", 
+        apellido: "Gómez", 
+        edad: 34, 
+        anioNacimiento: 1988,
+        altura: 1.67, 
+        clubActual: "Sevilla", 
+        clubDebut: "Arsenal de Sarandí",
+        paisDebut: "Argentina",
+        anioDebut: 2005,
+        debutSeleccion: 2017,
+        partidosSeleccion: 15,
+        golesSeleccion: 3,
+        partidosMundiales: 0,
+        posicion: "Mediocampista",
+        dorsalMundialista:24
+    },
+    {
+        nombre: "Enzo", 
+        apellido: "Fernández", 
+        edad: 21, 
+        anioNacimiento: 2001,
+        altura: 1.78, 
+        clubActual: "Benfica", 
+        clubDebut: "River Plate",
+        paisDebut: "Argentina",
+        anioDebut: 2020,
+        debutSeleccion: 2022,
+        partidosSeleccion: 2,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion: "Mediocampista",
+        dorsalMundialista:20
+    },
+    {
+        nombre: "Exequiel", 
+        apellido: "Palacios", 
+        edad: 24, 
+        anioNacimiento: 1998,
+        altura: 1.77, 
+        clubActual: "Bayern Leverkusen", 
+        clubDebut: "River Plate",
+        paisDebut: "Argentina",
+        anioDebut: 2015,
+        debutSeleccion: 2018,
+        partidosSeleccion: 20,
+        golesSeleccion: 0,
+        partidosMundiales: 0,
+        posicion: "Mediocampista",
+        dorsalMundialista:14
+    },
+    {
+        nombre: "Ángel", 
+        apellido: "Di María", 
+        edad: 34, 
+        anioNacimiento: 1988,
+        altura: 1.80, 
+        clubActual: "Juventus", 
+        clubDebut: "Rosario Central",
+        paisDebut: "Argentina",
+        anioDebut: 2005,
+        debutSeleccion: 2008,
+        partidosSeleccion: 123,
+        golesSeleccion: 25,
+        partidosMundiales: 13,
+        posicion: "Delantero",
+        dorsalMundialista:11
+    },
+    {
+        nombre: "Lautaro", 
+        apellido: "Martínez", 
+        edad: 25, 
+        anioNacimiento: 1997,
+        altura: 1.74, 
+        clubActual: "Inter", 
+        clubDebut: "Racing Club",
+        paisDebut: "Argentina",
+        anioDebut: 2015,
+        debutSeleccion: 2018,
+        partidosSeleccion: 40,
+        golesSeleccion: 21,
+        partidosMundiales: 0,
+        posicion: "Delantero",
+        dorsalMundialista:22
+    },
+    {
+        nombre: "Julián", 
+        apellido: "Álvarez", 
+        edad: 22, 
+        anioNacimiento: 2000,
+        altura: 1.70, 
+        clubActual: "Manchester City", 
+        clubDebut: "River Plate",
+        paisDebut: "Argentina",
+        anioDebut: 2018,
+        debutSeleccion: 2021,
+        partidosSeleccion: 11,
+        golesSeleccion: 2,
+        partidosMundiales: 0,
+        posicion: "Delantero",
+        dorsalMundialista:9
+    },
+    {
+        nombre: "Paulo", 
+        apellido: "Dybala", 
+        edad: 30, 
+        anioNacimiento: 1993,
+        altura: 1.77, 
+        clubActual: "Roma", 
+        clubDebut: "Instituto de Córdoba",
+        paisDebut: "Argentina",
+        anioDebut: 2011,
+        debutSeleccion: 2015,
+        partidosSeleccion: 34,
+        golesSeleccion: 3,
+        partidosMundiales: 1,
+        posicion: "Delantero",
+        dorsalMundialista:21
+    },
+    {
+        nombre: "Nicolás", 
+        apellido: "González", 
+        edad: 24, 
+        anioNacimiento: 1998,
+        altura: 1.80, 
+        clubActual: "Fiorentina", 
+        clubDebut: "Argentinos Juniors",
+        paisDebut: "Argentina",
+        anioDebut: 2016,
+        debutSeleccion: 2019,
+        partidosSeleccion: 21,
+        golesSeleccion: 3,
+        partidosMundiales: 0,
+        posicion: "Delantero",
+        dorsalMundialista:15
+    },
+    {
+        nombre: "Joaquín", 
+        apellido: "Correa", 
+        edad: 28, 
+        anioNacimiento: 1994,
+        altura: 1.88, 
+        clubActual: "Inter", 
+        clubDebut: "Estudiantes de La Plata",
+        paisDebut: "Argentina",
+        anioDebut: 2012,
+        debutSeleccion: 2017,
+        partidosSeleccion: 18,
+        golesSeleccion: 3,
+        partidosMundiales: 0,
+        posicion: "Delantero",
+        dorsalMundialista:16
+    },
+    {
+        nombre: "Lionel", 
+        apellido: "Messi", 
+        edad: 35, anioNacimiento: 1987,
+        altura: 1.69, 
+        clubActual: "París Saint-Germain", 
+        clubDebut: "Barcelona",
+        paisDebut: "España",
+        anioDebut: 2004,
+        debutSeleccion: 2005,
+        partidosSeleccion: 164,
+        golesSeleccion: 90,
+        partidosMundiales: 19,
+        posicion: "Delantero",
+        dorsalMundialista:10
+    },
+]
+
+//Buscar por el número de camiseta, devuelve número + nombre y apellido
+let buscarPorNumero = seleccionados.find(seleccionado=>seleccionado.dorsalMundialista===10)
+console.log(buscarPorNumero.dorsalMundialista + ": " + buscarPorNumero.nombre + " " + buscarPorNumero.apellido)
+
+//Sumatoria de partidos, goles y partidos jugados en mundiales
+let partidosTotalesEnSeleccion = seleccionados.reduce((acum, seleccionados)=>acum + seleccionados.partidosSeleccion, 0)
+let golesTotalesEnSeleccion = seleccionados.reduce((acum, seleccionados)=>acum + seleccionados.golesSeleccion, 0)
+let partidosTotalesEnMundiales = seleccionados.reduce((acum, seleccionados)=>acum + seleccionados.partidosMundiales, 0)
+console.log("Entre todos los jugadores suman " + partidosTotalesEnSeleccion + " partidos, " + golesTotalesEnSeleccion + " goles y " + partidosTotalesEnMundiales + " partidos en mundiales para la Selección Argentina")
+
+//Jugadores con al menos un partido jugado en mundiales en
+let jugaronMundial = seleccionados.filter(seleccionado=>seleccionado.partidosMundiales>0)
+console.log(jugaronMundial)
+
+//Indica el año del debut de cada jugador en la selección 
+seleccionados.forEach((seleccionado)=>{
+    console.log(seleccionado.nombre + " " + seleccionado.apellido + ": " + seleccionado.partidosSeleccion)
+})
+
+// Ordenar por el dorsal que van a usar en el mundial
+seleccionados.sort((a,b)=>a.dorsalMundialista-b.dorsalMundialista)
+console.log(seleccionados)
+
+// Nuevo array solo con información de los jugadores en la selección
+let seleccionadosEnSeleccion = seleccionados.map((seleccionado)=>{
+    return {nombre: seleccionado.apellido + ", " + seleccionado.nombre, dorsalMundialista: seleccionado.dorsalMundialista, debutSeleccion: seleccionado.debutSeleccion, partidosSeleccion: seleccionado.partidosSeleccion, golesSeleccion: seleccionado.golesSeleccion, partidosMundiales: seleccionado.partidosMundiales}
+})
+console.log(seleccionadosEnSeleccion)
+// Agregamos una nueva propiedad al array
+seleccionadosEnSeleccion = seleccionados.map((seleccionado)=>{
+    return {nombre: seleccionado.apellido + ", " + seleccionado.nombre, dorsalMundialista: seleccionado.dorsalMundialista, debutSeleccion: seleccionado.debutSeleccion, partidosSeleccion: seleccionado.partidosSeleccion, golesSeleccion: seleccionado.golesSeleccion, partidosMundiales: seleccionado.partidosMundiales, partidosMundialQatar: 0}
+})
+console.log(seleccionadosEnSeleccion)
+
+//
+
+seleccionadosEnSeleccion = seleccionados.map(seleccionado=>{
+    let debutoEnArgentina = false
+    if (seleccionado.paisDebut ==="Argentina"){
+        debutoEnArgentina = true
+    }
+    return {nombre: seleccionado.apellido + ", " + seleccionado.nombre, dorsalMundialista: seleccionado.dorsalMundialista,debutoEnArgentina: debutoEnArgentina, debutSeleccion: seleccionado.debutSeleccion, partidosSeleccion: seleccionado.partidosSeleccion, golesSeleccion: seleccionado.golesSeleccion, partidosMundiales: seleccionado.partidosMundiales, partidosMundialQatar: 0}
+})
+console.log(seleccionadosEnSeleccion)
+
+//Buscar si el jugador tiene más de 5 goles en la selección
+let dorsal = parseInt(prompt("Ingrese un dorsal del 1 al 26 para saber si el jugador tiene más de 5 goles en la selección"))
+let jugadorBuscado = seleccionados.find(seleccionado=>seleccionado.dorsalMundialista===dorsal)
+function masDe(g){
+    return(m)=>m>g
+}
+let masDe5Goles = masDe(5)
+console.log(masDe5Goles(jugadorBuscado.golesSeleccion))
+
+function informacion(){
+    let apellidoJugador = parseInt(prompt("Ingrese un dorsal del 1 al 26"))
+    let buscarApellido = seleccionados.find(seleccionado=>seleccionado.dorsalMundialista===apellidoJugador)
+    let datoASaber = parseInt(prompt("Para saber cuántos partidos tiene en la selección escriba 1 \n Para saber cuántos goles tiene en la selección escriba 2"))
+    if (datoASaber===1){
+        console.log(buscarApellido.apellido + " tiene " + buscarApellido.partidosSeleccion + " partidos en la selección")
+    } else if (datoASaber===2){
+        console.log(buscarApellido.apellido + " tiene " + buscarApellido.golesSeleccion + " goles en la selección")
+    } else {
+        console.log("Ingrese un dato válido")
+    }
+}
+informacion()
+
+//Promedio de goles/partido
+function promedio(){
+    let apellidoJugador = parseInt(prompt("Ingrese un dorsal del 1 al 26"))
+    let buscarApellido = seleccionados.find(seleccionado=>seleccionado.dorsalMundialista===apellidoJugador)
+    let golesPartido = buscarApellido.golesSeleccion/buscarApellido.partidosSeleccion
+    golesPartido = golesPartido.toFixed(2)
+    if (buscarApellido.golesSeleccion>0){
+        console.log(buscarApellido.apellido + " tiene un promedio de " + golesPartido + " goles por partido jugado en la selección")
+    } else{
+        console.log(buscarApellido.apellido + " no tiene goles en " + buscarApellido.partidosSeleccion + " partidos jugados en la selección")
+    }
+}
+promedio()
